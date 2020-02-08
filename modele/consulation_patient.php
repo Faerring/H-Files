b52c0083b6f3dbd1f65c88f736cda336654f875a
linhye <46298752+linhye@users.noreply.github.com>
2020-02-08 17:52:52 +0100

    Update consulation_patient.php
    Mise à jour

--- modele/consulation_patient.php
+++ modele/consulation_patient.php
@@ -6,20 +6,18 @@
 
     public function viewPData($patientID){
         require('connexionBDD.php');
-        if (isset($_POST['IDHosp']) && ($_POST['IDHosp'] == $patientID) && ($_POST['IDHosp'] != '')){
-            $y = "SELECT * FROM dmp_patient WHERE UUID LIKE ".$patientID;
-            $donneesP = $dbh->query($y);
-            return $donneesP;
-        }
+        $y = "SELECT * FROM dmp_patient WHERE UUID LIKE ".$patientID;
+        $donneesP = $dbh->query($y);
+        return $donneesP;
+        
     }
 
     function getAllergies($patientID){
         require('connexionBDD.php');
-        if (isset($_POST['IDHosp']) && ($_POST['IDHosp'] == $patientID) && ($_POST['IDHosp'] != '')){
-            $y = "SELECT * FROM antecedent WHERE UUID LIKE ".$patientID;
-            $antecedent = $dbh->query($y);
-            return $antecedent;
-        }
+        $y = "SELECT * FROM antecedent WHERE UUID LIKE ".$patientID;
+        $antecedent = $dbh->query($y);
+        return $antecedent;
+        
         
     }
 ?>
