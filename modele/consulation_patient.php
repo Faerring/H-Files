45240857924f2aba9a75583945ea5ee94c3b1bc7
linhye <46298752+linhye@users.noreply.github.com>
2020-02-08 17:33:18 +0100

    Update consulation_patient.php

--- modele/consulation_patient.php
+++ modele/consulation_patient.php
@@ -9,6 +9,17 @@
         if (isset($_POST['IDHosp']) && ($_POST['IDHosp'] == $patientID) && ($_POST['IDHosp'] != '')){
             $y = "SELECT * FROM dmp_patient WHERE UUID LIKE ".$patientID;
             $donneesP = $dbh->query($y);
+            return $donneesP;
         }
+    }
+
+    function getAllergies($patientID){
+        require('connexionBDD.php');
+        if (isset($_POST['IDHosp']) && ($_POST['IDHosp'] == $patientID) && ($_POST['IDHosp'] != '')){
+            $y = "SELECT * FROM antecedent WHERE UUID LIKE ".$patientID;
+            $antecedent = $dbh->query($y);
+            return $antecedent;
+        }
+        
     }
 ?>
