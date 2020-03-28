a31f22da9fa6298e876e4e99d38df1a9701efbff
Viriathe <51057912+Viriathe@users.noreply.github.com>
2020-03-28 16:23:27 +0100

    Add files via upload

--- controleur/modifUser.php
+++ controleur/modifUser.php
@@ -1,11 +1,27 @@
 <?php
+include("../vue/SqueletteDePage.php");
 include("../modele/utilisateur.php");
-$user = new Utilisateur;
-$newMail = $_POST['mail'];
-$newTel = $_POST['tel'];
+require('../modele/connexion.php');
+session_start();
+//empêcher l'accès à la page sans s'être authentifié
+if (!(isset($_SESSION['user']))){
+    echo "<script language='javascript'>alert('Bien tenté.');</script>";
+    header("refresh:0;url=../controleur/loginpage.php");
+}
+$user = $_SESSION['user'];
+debSquelette();
+if ($_POST['tel'] != ""){
+    $newTel = $_POST['tel'];
+    $user->setMobilePhone($newTel,$dbh);
+    echo "Modification du téléphone enregistrée";
+}
 
-$user->setMail($newMail);
-$user->setMobilePhone($newTel);
+if ($_POST['mail'] != ""){
+    $newMail = $_POST['mail'];
+    $user->setMail($newMail,$dbh);
+    echo "Modification du mail enregistrée";
+}
+
 
 
 
